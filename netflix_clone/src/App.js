85c5be5b6826f6127a614b7da3f9188dac40aa23
Dalpat Singh <dsdhaka25@gmail.com>
2023-06-28 00:29:59 +0530

    Chattapp with other chatapp project with netflix project

---
+++ netflix_clone/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Banner from './Componets/Banner';
+import Row from "./Componets/Row"
+import request from './requests'
+import Navbar from './Componets/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Banner/> 
+      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} isLargeRow />
+      <Row title="Trending now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
